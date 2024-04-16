0e8f686a7b8c6262c78ecd37431bc32ea62406e5
yossuli <s1f102302348@iniad.org>
2024-04-16 23:03:02 +0900

    update: import useState

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
+  const [board, setBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
   return (
     <div className={styles.container}>
       <div className={styles.boardStyle}>
