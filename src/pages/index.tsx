b2371ffc4234133c8681a7c6131783c4c401db08
yossuli <s1f102302348@iniad.org>
2024-04-16 20:31:59 +0900

    cerate: boardStyle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import styles from './index.module.css';
 
 const Home = () => {
-  return <div className={styles.container}>a</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.boardStyle}>
+        <div />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
